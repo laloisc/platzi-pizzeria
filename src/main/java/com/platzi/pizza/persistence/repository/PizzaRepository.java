1a7696d59a6fc72d4f6a8f855e39352e044e6a93
devlalo <lalo1isc@gmail.com>
2025-07-31 18:52:20 -0600

    Query Methods en Spring para Consultas Personalizadas

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -3,6 +3,9 @@ package com.platzi.pizza.persistence.repository;
 import com.platzi.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+import java.util.List;
 
+public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
 }
